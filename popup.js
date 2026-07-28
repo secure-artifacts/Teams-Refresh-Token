824c983795e135cebf1e4e316d7306175508d8d8
Raz1ner <63853884+dev-coco@users.noreply.github.com>
2026-07-27 20:50:51 -0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 const scriptUrl = document.getElementById('scriptUrl')
+const status = document.getElementById('status')
 
 scriptUrl.addEventListener('change', () => {
   chrome.storage.local.set({ scriptUrl: scriptUrl.value })
@@ -6,4 +7,28 @@ scriptUrl.addEventListener('change', () => {
 
 chrome.storage.local.get('scriptUrl', result => {
   scriptUrl.value = result.scriptUrl || ''
+})
+
+async function getToken () {
+  const cookies = await new Promise(resolve => chrome.cookies.getAll({ domain: 'live.com' }, cookies => resolve(cookies)))
+  const token = cookies.filter(x => x.name === 'skypetoken' && x.value.startsWith('B'))[0].value.replace(/^Bearer%3D|%26O.+/g, '')
+  return token
+}
+
+document.getElementById('update').addEventListener('click', async () => {
+  // 未设置 API 链接
+  const apiUrl = scriptUrl.value || ''
+  if (!apiUrl) return
+
+  status.innerText = '正在更新...'
+
+  const token = await getToken()
+  const body = new FormData()
+  body.append('token', token)
+  await fetch(apiUrl, {
+    body,
+    method: 'POST'
+  }).then(response => response.text())
+
+  status.innerText = '✅ 已更新'
 })
